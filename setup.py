466cdc7391ece092828e7912a68c4b3b646350f5
t-willi <twilli.gnl@gmail.com>
2022-11-27 16:09:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="1to7_ecg_generator", # Replace with your own username
+    version="1.1.2",
+    author="Tobias Willi",
+    author_email="twilli.gnl@gmail.com",
+    description="translates lead 1 ECG to 12 lead ECG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/simulamet-host/ecg_1_to_8/tree/side",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={'deepfakeecg': ['checkpoints/g_stat.pt']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'pandas',
+        "torch",
+  ],
+)
