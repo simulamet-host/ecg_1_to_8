38957769685701948da9de557f95b6ae45099cad
t-willi <twilli.gnl@gmail.com>
2022-11-30 12:45:05 +0100

    Update ecg_loss.py

--- Visualization/ecg_loss.py
+++ Visualization/ecg_loss.py
@@ -46,7 +46,7 @@ def get_lists(input_dir):
 
 #all_loss_list,all_loss_over_time_list,all_lead_pair_list=get_lists("C:/Users/tobia/Desktop/Simula/ecg_gen/output")
 
-def overlapping_leads(version=None,lead_pairs=None,path=None):
+def overlapping_leads(title="_",version=None,lead_pairs=None,path=None):
   fig,axs=plt.subplots(12,sharex=True,sharey=True,figsize=(15,8))
   plt.ylabel("mV",x=0.5,y=7.5)
   plt.xlabel("timesteps")
@@ -57,31 +57,38 @@ def overlapping_leads(version=None,lead_pairs=None,path=None):
     axs[k].plot(real.tolist(),label="real",linewidth=0.5,c="g",alpha=1)
     axs[k].plot(synth.tolist(),label="prediction",linewidth=0.5,c="r",alpha=0.5)
   plt.legend(loc=1, bbox_to_anchor=(1,15.7))
-  plt.savefig(f"{save_dir}/overlap_{version}.pdf", format="pdf", bbox_inches="tight")
+  plt.savefig(f"{save_dir}/{title}_overlap_{version}.pdf", format="pdf", bbox_inches="tight")
   plt.close()
 
     
-def plot_ECG_loss_over_time(input=None,path=None,version=None):
+def plot_ECG_loss_over_time(title="_",input=None,path=None,version=None,label=None):
   """
   enter a list of losses over time 
   """
   save_dir=str(Path(path).joinpath("loss_visual"))
   os.makedirs(save_dir,exist_ok=True)
-  fig, axs = plt.subplots(12,figsize=(15,6))
+  fig, axs = plt.subplots(12,sharex=True,sharey=True,figsize=(15,8))
+  plt.ylabel("MSEloss",x=0.5,y=7.5)
+  plt.xlabel("timesteps")
+  plt.title("Loss between real and predicted lead pairs I,II,III,aVR,aVL,aVF,v1,v2,v3,v4,v5,v6",x=0.5,y=14.5)
   for k,item in enumerate(input):
-    axs[k].plot(item.squeeze())
-  plt.savefig(f"{save_dir}/loss_over_time_{version}.pdf", format="pdf", bbox_inches="tight")
+    loss_label=str(round(label[k].item(),4))
+    axs[k].plot(item.squeeze(),label="average_loss:"+loss_label)
+    axs[k].legend(loc='upper right')
+  #plt.legend(loc=1, bbox_to_anchor=(1,15.7))
+  plt.savefig(f"{save_dir}/{title}_LossOverTime_{version}.pdf", format="pdf", bbox_inches="tight")
 
 
 #plot_ECG_loss_over_time(loss_over_time_list)
 
-def ecgloss_visual(input_dir,output_dir):
+def ecgloss_visual(title,input_dir,output_dir):
     x,y,z=get_lists(input_dir)
     for k,sub in enumerate(z):
-        overlapping_leads(version=k,lead_pairs=sub,path=output_dir)
-    for k,sub in enumerate(y):
-        plot_ECG_loss_over_time(input=sub,path=output_dir,version=k)
+        overlapping_leads(title,version=k,lead_pairs=sub,path=output_dir)
+    for loss in x:
+      for k,sub in enumerate(y):
+        plot_ECG_loss_over_time(title,input=sub,path=output_dir,version=k,label=loss)
 
 
 input_dir="C:/Users/tobia/Desktop/Simula/ecg_gen/output"
-ecgloss_visual(input_dir=input_dir,output_dir=input_dir)
+ecgloss_visual(title="test",input_dir=input_dir,output_dir=input_dir)
